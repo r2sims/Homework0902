a4b0d66aed48fa7de53d4cb5361260cf4573a0de
Rodrick Sims <rsimstransport@gmail.com>
2021-07-29 15:53:10 -0400

    updates

--- index.js
+++ index.js
@@ -1,13 +1,97 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
+//init();
+
+const inq = require('inquirer');
+const generateMarkdown = require('./generateMarkdown.js');
+const fs = require('fs');
+
+//    <!-- Questions -->
+//const questions = [
+
+    const terminalQuestions = () => {
+        inquirer.prompt([
+
+
+
+    {
+        type:'input',
+        name:'github',
+        message:'What is your github username?',
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'What is your email address?',
+    },
+    {
+        type:'input',
+        name:'title',
+        message:'What is the title of your project?',
+    },
+    {
+        type:'input',
+        name:'details',
+        message:'What are the details of your project?',
+        
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'What are the steps for installation?',
+    },
+    {
+        type:'input',
+        name:'benefits',
+        message:'What are some benefits for the use of your project?',
+    },
+    {
+        type:'list',
+        name:'licenses',
+        message:'What type of license(s) was chosen for your project?',
+        choices:['MIT License', "APACHE 2.0", "None"]
+    },
+    {
+       type:'input',
+       name:'contributions',
+       message:'Were there any contributions and/or collaborators to your project',
+    },
+    {
+      type:'input',
+      name:'test',
+      message:'What steps were taken to test project?',
+   },
+//]
+    //function init() {
+      //inq.prompt(questions).then((data) => {
+          //console.log(data)
+      //fs.writeFile('GeneratedREADME/GeneratedREADME.md', generateMarkdown(data),(err) => err? console.error(err) : console.log("Success!"))})
+   //}
+
+//init();
+
+]).then((data) => {
+    console.log(data);
+    const content = generateMarkdown(data);
+    fs.writeFile('README.md', content, err => {
+        if(err) console.log(err);
+        else console.log('Success!');
+    });
+});
+}
+
+function init() {
+terminalQuestions()
+}
+
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+//function generateMarkdown(data) {
+  //return `# ${data.title}
+
+//`;
+//}
+
+//module.exports = generateMarkdown;
+
 function generateMarkdown(data) {
-  return `# ${data.title}
+  if (data.licenses === "MIT License") {
+    data.licenses = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  }
+  if (data.licenses === "APACHE 2.0") {
+    data.licenses = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  }
 
-`;
-}
-
-module.exports = generateMarkdown;
+    
+  return `# Table of Contents
+  1. [Github](#Github)
+  2. [Email](#Email)
+  3. [Title](#Title)
+  4. [Details](#Details)
+  5. [Installation](#Installation)
+  6. [Benefits](#Benefits)
+  7. [Licenses](#Licenses)
+  8. [Contributions](#Contributions)
+  9. [Test](#Test)
+  
+  # ${data.title}
+# Details
+${data.details}
+# Installation
+${data.installation}
+# Benefits
+${data.benefits}
+# Licenses
+${data.licenses}
+# Contributions
+${data.contributions}
+# Test
+${data.test}
+# Github
+${data.github}
+# Email
+${data.email}
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
